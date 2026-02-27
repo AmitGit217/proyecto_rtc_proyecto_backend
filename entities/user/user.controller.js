798e653b909d3eac5199f66ba9fc601a21957eaf
AmitGit217 <amit217@yandex.com>
2026-02-27 22:22:49 +0100

    feat: user controller

---
+++ entities/user/user.controller.js
@@ -0,0 +1,61 @@
+import User from "./user.model";
+
+export const createUser = async (req, res) => {
+    try {
+        const { userName, email, password, role, image } = req.body;
+        const newUser = new User({ userName, email, password, role, image });
+        await newUser.save();
+        return res.status(201).json(newUser);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }  
+};
+
+export const getUsers = async (req, res) => {
+    try {
+        const users = await User.find().populate('posts');
+        return res.status(200).json(users);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+export const getUserById = async (req, res) => {
+    try {
+        const { id } = req.params;  
+        const user = await User.findById(id).populate('posts');
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        return res.status(200).json(user);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+export const updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const updatedUser = await User.findByIdAndUpdate(id, req.body, { new: true });
+        if (!updatedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        return res.status(200).json(updatedUser);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};
+
+export const deleteUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deletedUser = await User.findByIdAndDelete(id);
+        if (!deletedUser) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        return res.status(200).json({ message: 'User deleted successfully' });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+};  
+
--- main.js
+++ main.js
@@ -12,6 +12,9 @@ connectDB();
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
 });
+app.use((error, req, res, next) => {
+    return res.status(error.status || 500).json(error.message || 'Unexpected error')
+})
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3000;
