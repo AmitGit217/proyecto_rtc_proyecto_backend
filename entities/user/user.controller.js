7ecfee3d7dcd69c2c769f13e5f0c159ba0dddfd9
AmitGit217 <amit217@yandex.com>
2026-03-26 20:47:09 +0100

    fix: await in loginUser bcrypt compare

--- entities/user/user.controller.js
+++ entities/user/user.controller.js
@@ -28,7 +28,7 @@ export const loginUser = async (req, res, next) => {
         if (!user) {
             return res.status(404).json({ message: 'User not found' });
         }
-        const isMatch = bcrypt.compare(password, user.password);
+        const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return res.status(400).json({ message: 'Invalid credentials' });
         }
