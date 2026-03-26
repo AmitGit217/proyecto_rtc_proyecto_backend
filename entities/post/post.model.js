a070d984bbf537c05aa19fd5ff706ceaa495b7ec
AmitGit217 <amit217@yandex.com>
2026-03-26 20:42:12 +0100

    fix: require - required in modesl

--- entities/post/post.model.js
+++ entities/post/post.model.js
@@ -2,8 +2,8 @@ import mongoose from 'mongoose';
 
 
 const postSchema = new mongoose.Schema({
-    title: {type:String, require: true, trim: true},
-    description: { type: String, require: true, trim: true },   
+    title: {type:String, required: true, trim: true},
+    description: { type: String, required: true, trim: true },   
     owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "users",
--- entities/user/user.model.js
+++ entities/user/user.model.js
@@ -4,11 +4,11 @@ import bcrypt from 'bcrypt';
 
 
 const userSchema = new mongoose.Schema({
-    userName: {type:String, require: true, trim: true},
-    email: { type: String, require: true, trim: true, unique: true },
-    password: { type: String, require: true, trim: true },
-    role: { type: String, require: true, trim: true },
-    image: { type: String, require: false, trim: true },
+    userName: {type:String, required: true, trim: true},
+    email: { type: String, required: true, trim: true, unique: true },
+    password: { type: String, required: true, trim: true },
+    role: { type: String, required: true, trim: true },
+    image: { type: String, required: false, trim: true },
 },{
     timestamps: true,
 })
