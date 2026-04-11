00ee0903314cb6cbb4512d13059e4b8d35d7a7db
AmitGit217 <amit217@yandex.com>
2026-04-11 23:34:02 +0200

    fix: strict isAdmin rule

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -1,19 +1,11 @@
 const isAdmin = (req, res, next) => {
-  const isAdmin = req.user?.role === 'admin';
-
-  if (isAdmin) return next();
-
-  if (req.method === 'DELETE') {
-    if (req.user._id.toString() !== req.params.id) {
-      return res.status(403).json("Forbidden");
-    }
-    return next();
+  if (!req.user) {
+    return res.status(401).json("Unauthorized");
   }
 
-  if (req.body?.role === 'admin') {
+  if (req.user.role !== 'admin') {
     return res.status(403).json("Forbidden");
   }
 
   next();
 };
-export default isAdmin;
