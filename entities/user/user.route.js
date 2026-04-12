53335c9879d747a54db723fb27ecac68b16d739e
AmitGit217 <amit217@yandex.com>
2026-04-12 21:30:57 +0200

    fix: isAdminOrOwner import

--- entities/user/user.route.js
+++ entities/user/user.route.js
@@ -3,13 +3,13 @@ const UserRoutes = express.Router();
 import { createUser, getUsers, getUserById, updateUser, deleteUser, loginUser } from './user.controller.js';
 import {upload} from '../../config/fileStorage.js';
 import isAuth from '../../middlewares/auth.js';
-import isAdminOrOwner from '../../middlewares/isAdminOrOwner.js';
+import isAdmin from '../../middlewares/isAdminOrOwner.js';
 
 UserRoutes.post('/', upload.single('image'), createUser);
 UserRoutes.get('/', getUsers);
 UserRoutes.get('/:id', getUserById);
-UserRoutes.put('/:id', isAuth, isAdminOrOwner , upload.single('image'), updateUser);
-UserRoutes.delete('/:id', isAuth, isAdminOrOwner, deleteUser);
+UserRoutes.put('/:id', isAuth, isAdmin , upload.single('image'), updateUser);
+UserRoutes.delete('/:id', isAuth, isAdmin, deleteUser);
 UserRoutes.post('/login', loginUser);
 
 export default UserRoutes;
