c44c8af29164ee87ad0f035761fd6f1151652e00
AmitGit217 <amit217@yandex.com>
2026-04-28 22:20:53 +0200

    fix: update role normal user block

--- middlewares/isAdminOrOwner.js
+++ middlewares/isAdminOrOwner.js
@@ -7,6 +7,9 @@ const isAdminOrOwner = (req, res, next) => {
   const isOwner = req.user._id.toString() === req.params.id;
 
   if (isAdmin || isOwner) {
+    if (isOwner && req.body.role && req.body.role !== 'user') {
+      return res.status(403).json("Forbidden");
+    }
     return next();
   }
 
